c93052c4fbe27c7c1bb0b59c1c3a2e17791b40ee
msivtronic <msivtronic@gmail.com>
2021-01-18 10:36:50 -0300

    Add more functionality

--- GitExerc/Program.cs
+++ GitExerc/Program.cs
@@ -8,6 +8,21 @@ namespace GitExerc
         {
             Console.WriteLine("Hello World!");
 
+            int hour = DateTime.Now.Hour;
+
+            if ( hour < 12)
+            {
+                Console.WriteLine("Good Morning!");
+            }
+            else if(hour < 18)
+            {
+                Console.WriteLine("Good Afternoon!");
+            }
+            else
+            {
+                Console.WriteLine("Good Night!");
+            }
+
             Console.ReadLine();
         }
     }
